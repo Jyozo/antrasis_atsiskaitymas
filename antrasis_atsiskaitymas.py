2ba44feaf03a294190b72cc009f9e0b34545ba6c
Jyozo <gedvrk@gmail.com>
2014-04-10 23:33:31 +0300

    Removed dummy function. Added functionality to read directory path from command line parameter and use it to get a list of all the files in that directory. Pep8 test passed.

--- antrasis_atsiskaitymas.py
+++ antrasis_atsiskaitymas.py
@@ -1,2 +1,18 @@
-def dummy_function():
+import sys
+import os
+
+
+def read_words():
     pass
+
+path = sys.argv[1]
+
+if path[-1] is not '/':
+    path = path+'/'
+
+print 'Einamasis katalogas: '+path
+
+fileNames = [name for name in os.listdir(path)
+             if os.path.isfile(path+name)]
+
+print fileNames
