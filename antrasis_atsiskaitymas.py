abfcd56f172c7f4da3bc1a082dc31c07381a711e
Jyozo <gedvrk@gmail.com>
2014-04-11 00:12:08 +0300

    Implemented function read_words, that reads a file and scans for different words and their frequency. Returns a dictionary in this format: {word:frequency,...}. Pep8 test passed.

--- antrasis_atsiskaitymas.py
+++ antrasis_atsiskaitymas.py
@@ -2,17 +2,28 @@ import sys
 import os
 
 
-def read_words():
-    pass
+def read_words(fileName):
+    file = open(path+fileName)
+    wordcount = {}
+    for word in file.read().split():
+        if word not in wordcount:
+            wordcount[word] = 1
+        else:
+            wordcount[word] += 1
+    return wordcount
+
 
 path = sys.argv[1]
 
 if path[-1] is not '/':
     path = path+'/'
 
-print 'Einamasis katalogas: '+path
+print 'Specified directory: '+path
 
 fileNames = [name for name in os.listdir(path)
              if os.path.isfile(path+name)]
 
 print fileNames
+
+print 'Words from the file '+fileNames[0]
+print read_words(fileNames[0])
