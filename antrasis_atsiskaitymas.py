7265bc4687797d3ae9b5ab080335820a752f3177
Jyozo <gedvrk@gmail.com>
2014-04-11 11:33:54 +0300

    Modified function read_words to use regular expressions. Added function read_symbols to scan a file for symbols and their frequency. read_symbols returns a dictionary in this format: {symbol:frequency,...}. Not in Pep8 standard.

--- antrasis_atsiskaitymas.py
+++ antrasis_atsiskaitymas.py
@@ -1,16 +1,28 @@
 import sys
 import os
+import re
 
 
 def read_words(fileName):
     file = open(path+fileName)
     wordcount = {}
-    for word in file.read().split():
+    for word in re.findall("[\w']+",file.read()):
         if word not in wordcount:
             wordcount[word] = 1
         else:
             wordcount[word] += 1
     return wordcount
+
+
+def read_symbols(fileName):
+    file = open(path+fileName)
+    symbolcount = {}
+    for symbol in re.findall('\w',file.read()):
+        if symbol not in symbolcount:
+            symbolcount[symbol] = 1
+        else:
+            symbolcount[symbol] += 1
+    return symbolcount
 
 
 path = sys.argv[1]
@@ -27,3 +39,6 @@ print fileNames
 
 print 'Words from the file '+fileNames[0]
 print read_words(fileNames[0])
+
+print 'Symbols from the file '+fileNames[0]
+print read_symbols(fileNames[0])
